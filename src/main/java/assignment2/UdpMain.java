8f64b797c1b43b5128951d5eb54b807c1749a8e7
StavroG <stavrogorou@outlook.com>
2024-04-14 23:56:13 -0700

    Added TCP Client

--- src/main/java/assignment2/UdpMain.java
+++ src/main/java/assignment2/UdpMain.java
@@ -1,5 +1,44 @@
 package assignment2;
 
+import java.net.DatagramPacket;
+
 public class UdpMain
 {
+    private static final int PORT_NUMBER = 5000;
+    private static final String QUIT_COMMAND = "quit";
+    private static final String HUMOROUS_MESSAGE = "a very humorous message, please laugh";
+
+    public static void main(String[] args)
+    {
+        UdpServer udpServer = new UdpServer(PORT_NUMBER);
+
+        while(true)
+        {
+            try
+            {
+                DatagramPacket messagePacket = udpServer.listenForMessage();
+
+                String message = new String(messagePacket.getData(), 0, messagePacket.getLength());
+
+                System.out.println("Message received: " + message);
+
+                if(message.equalsIgnoreCase(QUIT_COMMAND))
+                {
+                    udpServer.closeServer();
+                    break;
+                }
+                else
+                {
+                    String modifiedInput = message + " - " + HUMOROUS_MESSAGE;
+                    udpServer.sendResponse(modifiedInput, messagePacket.getAddress(), messagePacket.getPort());
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println("An error occurred, disconnecting from client");
+                System.out.println("Error message: " + e.getMessage());
+                udpServer.closeServer();
+            }
+        }
+    }
 }
--- src/main/java/assignment2/UdpServer.java
+++ src/main/java/assignment2/UdpServer.java
@@ -1,5 +1,82 @@
 package assignment2;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class UdpServer
 {
+    private final DatagramSocket datagramSocket;
+
+    /**
+     * Start a UDP server on a given port
+     *
+     * @param portNumber the port to listen for messages on
+     */
+    public UdpServer(int portNumber)
+    {
+        try
+        {
+            System.out.println("Attempting to start server...");
+
+            datagramSocket = new DatagramSocket(portNumber);
+
+            System.out.println("Server started on port: " + portNumber);
+        }
+        catch(SocketException e)
+        {
+            throw new RuntimeException("Could not start server");
+        }
+    }
+
+    /**
+     * Listen to a UDP message from the client
+     *
+     * @return the data packet containing information such as: the message data, client address and client port number
+     */
+    public DatagramPacket listenForMessage()
+    {
+        try
+        {
+            byte[] buffer = new byte[256];
+            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+
+            datagramSocket.receive(datagramPacket);
+
+            return datagramPacket;
+        }
+        catch(IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Send a response to the client after getting a message
+     *
+     * @param response   the message to send back to the client
+     * @param address    the address of the client
+     * @param portNumber the port number the client is listening on
+     */
+    public void sendResponse(String response, InetAddress address, int portNumber)
+    {
+        try
+        {
+            datagramSocket.send(new DatagramPacket(response.getBytes(), response.getBytes().length, address, portNumber));
+        }
+        catch(IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Shutdown the server datagram socket when done
+     */
+    public void closeServer()
+    {
+        datagramSocket.close();
+    }
 }
